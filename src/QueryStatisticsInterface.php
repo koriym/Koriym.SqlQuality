99dd34e881cf6dbc69b04b01f1976b9384003799
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-01-14 22:10:51 +0900

    Add getQueryResults method to QueryStatisticsInterface
    Introduce a new method, getQueryResults, which provides query results as an associative array. This enhances the interface by offering a way to retrieve and handle raw query output data.

--- src/QueryStatisticsInterface.php
+++ src/QueryStatisticsInterface.php
@@ -7,6 +7,8 @@ namespace Koriym\SqlQuality;
 interface QueryStatisticsInterface
 {
     /**
+     * Performs a calculation and returns the result
+     *
      * @return array{
      *     total_count: int,
      *     avg_cost: float,
@@ -14,4 +16,11 @@ interface QueryStatisticsInterface
      * }
      */
     public function calculate(array $queryResults): array;
+
+    /**
+     * Gets the query results
+     *
+     * @return array<string, mixed> Query results as an associative array.
+     */
+    public function getQueryResults(): array;
 }
